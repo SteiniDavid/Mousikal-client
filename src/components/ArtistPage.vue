a93d4528239263be6eec94eddc771c213cd2b03d
Steini Davidsson <steini@mac.com>
2019-12-10 13:56:53 -0500

    cleaned stuff up

--- src/components/ArtistPage.vue
+++ src/components/ArtistPage.vue
@@ -60,39 +60,6 @@ export default {
   },
   data: () => ({
     artist: "",
-    artistAlbums: [
-      {
-        album: "Songs For Groovy Children: The Fillmore East Concerts",
-        imageURL:
-          "https://i.scdn.co/image/7e269ae7ce69e976f0a4ceed8d56de82c03f4410"
-      },
-      {
-        album:
-          "At Last...The Beginning - The Making Of Electric Ladyland: The Early Takes Sampler",
-        imageURL:
-          "https://i.scdn.co/image/ab67616d0000b273791919fe19e699093629643f"
-      },
-      {
-        album: "Freedom: Atlanta Pop Festival (Live)",
-        imageURL:
-          "https://i.scdn.co/image/ab67616d0000b2731a9909a62934d5a80a73152e"
-      },
-      {
-        album: "Miami Pop Festival",
-        imageURL:
-          "https://i.scdn.co/image/ab67616d0000b27366c6ff06b078ac4d76af7e0e"
-      },
-      {
-        album: "People, Hell & Angels",
-        imageURL:
-          "https://i.scdn.co/image/ab67616d0000b2733dbc906bf1b195a875bcfc97"
-      },
-      {
-        album: "Winterland",
-        imageURL:
-          "https://i.scdn.co/image/ab67616d0000b27382f614e9a1555d5aa78fbce1"
-      }
-    ],
     artistInfo: [],
     albumClickedOn: false,
     clickedAlbumID: '',
@@ -119,57 +86,5 @@ export default {
   }
 };
 
-// album: "Songs For Groovy Children: The Fillmore East Concerts"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/7e269ae7ce69e976f0a4ceed8d56de82c03f4410"
-// __proto__: Object
-// 1:
-// album: "At Last...The Beginning - The Making Of Electric Ladyland: The Early Takes Sampler"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b273791919fe19e699093629643f"
-// __proto__: Object
-// 2:
-// album: "Both Sides of the Sky"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b2731a9909a62934d5a80a73152e"
-// __proto__: Object
-// 3:
-// album: "Freedom: Atlanta Pop Festival (Live)"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b273f9f9cc10a1a532c3b6bc358a"
-// __proto__: Object
-// 4:
-// album: "Miami Pop Festival"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b27366c6ff06b078ac4d76af7e0e"
-// __proto__: Object
-// 5:
-// album: "People, Hell & Angels"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b2733dbc906bf1b195a875bcfc97"
-// __proto__: Object
-// 6:
-// album: "People, Hell & Angels"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b2734fdf251a3d05164e4a10c020"
-// __proto__: Object
-// 7:
-// album: "Winterland"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b27382f614e9a1555d5aa78fbce1"
-// __proto__: Object
-// 8:
-// album: "Winterland"
-// artist: "Jimi Hendrix"
-// artistID: "776Uo845nYHJpNaStv1Ds4"
-// imageURL: "https://i.scdn.co/image/ab67616d0000b2736a1f03cc603edf684f52dbd2"
 </script>
 
