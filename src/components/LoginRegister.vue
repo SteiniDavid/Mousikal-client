aea6a8b66dde51560b3e7ea63c2ed64fbfb1c29b
Steini Davidsson <steini@mac.com>
2019-12-10 14:35:41 -0500

    fixed bugs

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,52 +1,85 @@
 <template>
-  <div align='center'>
+  <div align="center">
     <div v-if="loggedIn">
-        <span>Logged in as </span> {{this.loggedInUserName}}
-
-        <v-btn class="mr-4" @click="logOut">Logout</v-btn>
-        <Search :loggedInUserName="this.loggedInUserName"/>
+      <span class="headline">Logged in as {{this.loggedInUserName}} </span>
+      <v-btn class="mr-4" @click="logOut">Logout</v-btn>
+      <Search :loggedInUserName="this.loggedInUserName" />
     </div>
 
     <div class="LoginRegistration" v-if="!loggedIn">
       <h1>Registration and Login</h1>
-      <br>
+      <br />
       <v-container>
         <v-layout row>
-           <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
           <v-row>
-            <v-card max-width=1000 min-width=400>
-            <v-card-title>Register</v-card-title>
-            <v-container>
-              <v-text-field clearable dense filled outlined v-model="name_register" label="Name" required></v-text-field>
-              <v-text-field clearable dense filled outlined v-model="password_register" type="'password'" label="Password" required></v-text-field>
-            </v-container>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn class="mr-4" @click="registerAccount">Register</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-row>
-        
-        <v-spacer></v-spacer>
+            <v-card max-width="1000" min-width="400">
+              <v-card-title>Register</v-card-title>
+              <v-container>
+                <v-text-field
+                  clearable
+                  dense
+                  filled
+                  outlined
+                  v-model="name_register"
+                  label="Name"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  clearable
+                  dense
+                  filled
+                  outlined
+                  v-model="password_register"
+                  type="'password'"
+                  label="Password"
+                  required
+                ></v-text-field>
+              </v-container>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="mr-4" @click="registerAccount">Register</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-row>
 
-        <v-row>
-        <v-card max-width=1000 min-width=400>
-          <v-card-title>Login</v-card-title>
-          <v-container>
-            <v-text-field clearable dense filled outlined v-model="name_login" label="Name" required></v-text-field>
-            <v-text-field clearable dense filled outlined v-model="password_login" :type="'password'" label="Password" required></v-text-field>
-          </v-container>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn class="mr-4" @click="loginAccount">Login</v-btn>
-            <v-spacer></v-spacer>
-          </v-card-actions>
-          </v-card>
+          <v-spacer></v-spacer>
+
+          <v-row>
+            <v-card max-width="1000" min-width="400">
+              <v-card-title>Login</v-card-title>
+              <v-container>
+                <v-text-field
+                  clearable
+                  dense
+                  filled
+                  outlined
+                  v-model="name_login"
+                  label="Name"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  clearable
+                  dense
+                  filled
+                  outlined
+                  v-model="password_login"
+                  :type="'password'"
+                  label="Password"
+                  required
+                ></v-text-field>
+              </v-container>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="mr-4" @click="loginAccount">Login</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
           </v-row>
         </v-layout>
       </v-container>
-      </div>
+    </div>
   </div>
 </template>
 
@@ -55,13 +88,13 @@ import { createAccount } from "../api/account/Account";
 import { login } from "../api/account/Account";
 import { getStatus } from "../api/account/Account";
 import { setToken } from "../config/Token";
-import Search from '@/components/Search.vue'
+import Search from "@/components/Search.vue";
 
 export default {
   name: "LoginRegister",
 
   components: {
-    Search,
+    Search
   },
   data: () => ({
     name_register: "",
@@ -92,10 +125,20 @@ export default {
         name: this.name_login,
         pass: this.password_login
       };
-      await login(namePass).then(
-        (this.loggedIn = true),
-        (this.loggedInUserName = this.name_login)
-      );
+      if (await login(namePass)) {
+          window.console.log('logged in'),
+          (this.loggedIn = true),
+          (this.loggedInUserName = this.name_login)
+        } else {
+          window.console.log('failed')
+        }
+
+
+      // await login(namePass).then(
+      //   window.console.log(res),
+      //   (this.loggedIn = true),
+      //   (this.loggedInUserName = this.name_login)
+      // );
     },
     async logOut() {
       setToken("");
